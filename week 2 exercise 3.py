12960b1db1a2101bfde2754e648bce30f3bd8280
ConnorMcde <26596059@edgehill.ac.uk>
2025-11-19 12:01:40 +0000

    Add files via upload

---
+++ week 2 exercise 3.py
@@ -0,0 +1,14 @@
+"""Week 2 exercise 3"""
+
+name = "Connor"
+course = "Computer science and ai"
+language = "Python"
+movie = "Kung fu panda 2"
+
+a = "hello"
+print(a + name)
+
+greeting = "hello {name}!"
+print(greeting.format(name = "connor"))
+
+print(f"Hello {name} do u do {course}!")
