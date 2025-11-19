d067fff8211adc9bb2151dfbf105f58340022268
ConnorMcde <26596059@edgehill.ac.uk>
2025-11-19 11:26:53 +0000

    Add files via upload

---
+++ week2_lab.py
@@ -0,0 +1,11 @@
+"""
+Python program that converts a temperature from Celsius to Fahrenheit.
+"""
+
+
+Celsius = int(input("Enter the Celsius: "))#represents 25 degrees celsius
+Fahrenheit = (Celsius * 9/5) + 32 #formula for conversion having the celsius times by 9/5 then adding 32
+
+print(f"Celsius is {Celsius} and Fahrenheit is {Fahrenheit}.")
+
+"""As Celsius increases so does the fahrenheit"""
