7ea028f2602a2555cd6f4c4656e2d5fbd1d92cd6
ConnorMcde <26596059@edgehill.ac.uk>
2025-11-19 11:40:38 +0000

    Add files via upload

---
+++ week 2 exercise 2.py
@@ -0,0 +1,20 @@
+"""Week 2 exercise 2"""
+
+
+my_string = "Word"# String
+my_integer = 10# Integer
+my_float = 3.14159# Float
+my_boolean = True# Boolean
+
+print("String variable:", my_string)
+print("Integer variable:", my_integer)
+print("Float variable:", my_float)
+print("Boolean variable:", my_boolean)
+
+print("Type of my_string:", type(my_string))
+print("Type of my_integer:", type(my_integer))
+print("Type of my_float:", type(my_float))
+print("Type of my_boolean:", type(my_boolean))
+
+my_integer = "forty-two"
+print("Reassigned my_integer:", my_integer, "| Type:", type(my_integer))
